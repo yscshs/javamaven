c63ff5821f65197fe8031088c7841dce786f7640
orange <111262330+yscshs@users.noreply.github.com>
2022-08-15 17:22:31 +0800

    Update GitTest.java

--- javase/src/main/java/com/javase/GitTest.java
+++ javase/src/main/java/com/javase/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hello Git!");
         System.out.println("main Git!");
         System.out.println("othertest Git!");
+        System.out.println("GitHub test!");
 
     }
 }
