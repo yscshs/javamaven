83064fa5cd30b4802d7f67b5f91ba9b2b951d1d6
Orange <293444447@qq.com>
2022-08-15 16:21:23 +0800

    first commit

---
+++ javase/src/main/java/com/javase/GitTest.java
@@ -0,0 +1,14 @@
+package com.javase;
+
+/**
+ * @Author: Orange
+ * @Program: javamaven
+ * @Description:
+ * @Date: 2022/8/15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+
+    }
+}
