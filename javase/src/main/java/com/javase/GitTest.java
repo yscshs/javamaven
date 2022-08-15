71d6c3cc5e63e9f7931a6fa3afbe36add28ab527
Orange <293444447@qq.com>
2022-08-15 16:23:06 +0800

    second commit

--- javase/src/main/java/com/javase/GitTest.java
+++ javase/src/main/java/com/javase/GitTest.java
@@ -9,6 +9,7 @@ package com.javase;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git!");
 
     }
 }
