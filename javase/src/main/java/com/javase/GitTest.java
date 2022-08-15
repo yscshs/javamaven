ecac885ab85bbec197114cb79a5e13379b419547
Orange <293444447@qq.com>
2022-08-15 16:29:24 +0800

    third commit

--- javase/src/main/java/com/javase/GitTest.java
+++ javase/src/main/java/com/javase/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("hello Git!");
+        System.out.println("hello Git!");
 
     }
 }
